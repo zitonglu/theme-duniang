508effa2da5483a13941a6ddd70cfbfd1b843bd1
zitonglu <910109610@qq.com>
2016-11-20 09:36:11 +0800

    404.php

--- template/404.php
+++ template/404.php
@@ -6,5 +6,13 @@
 </head>
 <body>
 	<script type="text/javascript" src="http://www.qq.com/404/search_children.js" charset="utf-8" homePageUrl="{$host}" homePageName="返回{$name}"></script>
+	<script>
+		window.onload=function changeURL() {
+			if (!document.getElementsByClassName('desc_link')) return true;
+			oldUrl=document.getElementsByClassName('desc_link');
+			oldUrl[0].setAttribute('href', "{$host}");
+			oldUrl[0].firstChild.nodeValue="返回{$name}";
+		}
+	</script>
 </body>
 </html>
